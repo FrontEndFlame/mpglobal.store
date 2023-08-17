e4433824ef283d47e6a51834c41696999513f400
FrontEndFlame <138845666+FrontEndFlame@users.noreply.github.com>
2023-08-17 21:20:57 +0300

    Add files via upload

---
+++ success.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html style="background: rgb(168, 0, 0);">
+<head>
+  <meta charset="utf-8">
+  <title>Заявка отправена!</title>
+  <link href="https://fonts.googleapis.com/css?family=Unbounded:200,300,regular,500,600,700,800,900" rel="stylesheet" />
+  <meta http-equiv="refresh" content="3;url=index.html">
+</head>
+<body>
+  <div style="
+        display: flex;
+        height: 100vh;
+        max-width: 80%;
+        margin: 0 auto;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+  ">
+    <h1 style="
+         font-family: 'Unbounded';
+         font-size: 4rem;
+         color: white;
+    ">
+        <span>Спасибо,</span>
+        <span>что оставили</span>
+        <span>заявку!</span>
+    </h1>
+    <p style="
+        font-family: 'Unbounded';
+        font-size: 2rem;
+        color: white;
+        text-align: center;
+    
+    ">Вы вернетесь на главную страницу<br> через 3 секунды...</p>
+  </div>
+</body>
+</html>
